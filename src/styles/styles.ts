a8e93ff4e6958a04bac29a2c479a1bf0425b429a
0xDV <di593297724ma@gmail.com>
2025-01-11 11:13:11 +0100

    fixed styles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -109,7 +109,7 @@ export const styles = StyleSheet.create({
     top: 0,
     left: 0,
     right: 0,
-    bottom: 0,
+    bottom: 10,
     backgroundColor: 'rgba(0,0,0,0.5)',
     zIndex: 1,
   },
