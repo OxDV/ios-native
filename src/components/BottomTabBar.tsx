ae67f258cb0b36f3a4cfff47e903cb06e0de3750
0xDV <di593297724ma@gmail.com>
2025-01-11 09:43:12 +0100

    increase size of bottom tabs

--- src/components/BottomTabBar.tsx
+++ src/components/BottomTabBar.tsx
@@ -59,7 +59,7 @@ export const BottomTabBar: React.FC<Props> = ({
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    height: 60,
+    height: 70,
     backgroundColor: '#fff',
     borderTopWidth: 1,
     borderTopColor: '#eee',
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     right: 0,
-    paddingTop: 5,
+    paddingTop: 10,
   },
   darkContainer: {
     backgroundColor: '#1a1a1a',
